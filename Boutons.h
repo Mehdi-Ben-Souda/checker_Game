1f068e8260cd7e46fb5fbec0e4dc3d2da80309e9
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-04-03 18:20:49 +0000

    Update Boutons.h

--- Boutons.h
+++ Boutons.h
@@ -9,16 +9,23 @@ typedef struct
 typedef struct
 {
     GtkWidget *button;//pointeur sur un bouton
-    char label[NB_Cara_titre];//le nom du bouton
-}Boutons;
-Boutons *Initialiser_Boutons(char label[NB_Cara_titre])
+    char label[maxcarac];//le nom du bouton
+}Boutons;//structure commun entre tous les boutons
+/*
+ * Fonction qui permet d'initialiser la structure Boutons
+ * entrées : -> le label du bouton
+ * sorties : ->Bouton initialisé
+*/
+Boutons *Initialiser_Boutons(char label[maxcarac])
 {
+    //allocation de la mémoire
     Boutons *NE= (Boutons*)malloc(sizeof (Boutons));
-    if(!NE)
+    if(!NE)//vérification d'allocation
     {
         printf("Erreur d'allocation !!!");
         exit(0);
     }
+    //initialistation des champs du structure
     strcpy(NE->label,label);
     NE->button=NULL;
     return ((Boutons*)NE);
