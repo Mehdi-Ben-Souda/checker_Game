283ff885aa508846a575060ff9a9d258aa2ce98c
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-04-04 02:56:16 +0000

    Add files via upload

---
+++ Macros.h
@@ -0,0 +1,15 @@
+#include <gtk/gtk.h>
+#include <libxml2/libxml/parser.h>
+#define maxcarac 50
+#include "common.h"
+#include "Fenetre.h"
+#include "FileChooser.h"
+#include "conteneurs.h"
+#include "Boutons.h"
+#include "Toggle_Simple_Bouton.h"
+#include "Radio_Check_Boutton.h"
+#include "Menu.h"
+#include "ToolBar.h"
+#include "InfoToolBar.h"
+#include "fonction_XML.h"
+#include "input_switch.h"
