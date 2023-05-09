7decd94ffa06c1f44d9673cff65f32187e528b34
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-05-09 15:19:28 +0100

    vv

--- CalculCout.h
+++ CalculCout.h
@@ -27,3 +27,36 @@ typedef struct mvt
     struct mvt *svt;
 }mouvement;
 
+
+pion *creer_pion()
+{
+    pion *NE=(pion*)malloc(sizeof (pion));
+    if(!NE)
+    {
+        printf("\nErreur d'allocation pion !!");
+        exit(0);
+    }
+    return ((pion*)NE);
+}
+match *creer_match()
+{
+    match *NE=(match*)malloc(sizeof (match));
+    if(!NE)
+    {
+        printf("\nErreur d'allocation pion !!");
+        exit(0);
+    }
+    return ((match*)NE);
+}
+historique *creer_cellule_historique(match M)
+{
+    historique *NE=(historique *)malloc(sizeof (historique));
+    if(!NE)
+    {
+        printf("\nErreur d'allocation pion !!");
+        exit(0);
+    }
+    NE->game=M;
+    NE->match_svt=NULL;
+    return ((historique *)NE);
+}
