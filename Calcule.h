63f23be6c7ad9c470c72982de39ed31f5c3b4923
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-05-24 13:55:11 +0100

    Update Calcule.h

--- Calcule.h
+++ Calcule.h
@@ -38,3 +38,68 @@ noeud *calcuer_cout(noeud *N)
     }
     return ((noeud*)N);
 }
+/*
+void Afficher_arbre(noeud *a)
+{
+    if(a)
+    {
+        noeud *tmp;
+
+        while (a->svt)
+    }
+
+}
+*/
+noeud *construire_niveau(noeud *N,int idj,pion Tab[24],int damier[NB_CASES][NB_CASES],int *i,int *niveau)
+{
+   if( *niveau <=2)
+   {
+       noeud *tmp,*tmp2;
+       mouvement * m,*ptc;
+       int k=0;
+       int copy_damier[NB_CASES][NB_CASES];
+       pion copy_lesjetons[NB_PIONS];
+       if((*i)>=0 && (*i)<24 )
+       {
+           if((idj ==1 && *i >11) || (idj ==-1 && *i<12))  return ((noeud*)N);
+
+           if(!N)
+           {
+               N=creeNoeud();
+               N=Mouvements_possible((*i),damier,Tab,N);
+               tmp=N;
+           }
+           else
+           {
+               tmp=N;
+               while (tmp->svt)
+                   tmp=tmp->svt;
+               tmp->svt=creeNoeud();
+               tmp->svt=Mouvements_possible((*i),damier,Tab,tmp->svt);
+               tmp=tmp->svt;
+           }
+           tmp2=tmp;
+
+           while(tmp2)
+           {
+               ptc=tmp2->lejeu;
+               copierLesJetons(copy_lesjetons, Tab);
+               copierDamier(copy_damier, damier);
+               while (ptc)
+               {
+                   m=creer_mouvement(ptc->x,ptc->y,ptc->IDj, ptc->IDa);
+                   deplacerJeton(*m, copy_damier, copy_lesjetons);
+                   ptc=ptc->svt;
+               }
+               (*niveau)++;
+               tmp2->sous_jeu=construire_niveau(tmp2->sous_jeu,(idj*(-1)),copy_lesjetons,copy_damier,&k,niveau);
+               tmp=tmp2;
+               tmp2=tmp2->svt;
+           }
+           (*i)++;
+           construire_niveau(tmp,idj,Tab,damier,i,niveau);
+       }
+   }
+    (*niveau)--;
+    return ((noeud*)N);
+}
