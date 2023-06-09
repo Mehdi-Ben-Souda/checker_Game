0df7128939659e19b4435110c5cf2f840dbb80cc
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-06-09 13:07:17 +0100

    Update mouvements_possibles.h

--- mouvements_possibles.h
+++ mouvements_possibles.h
@@ -3,21 +3,20 @@
 
 typedef struct noeud
 {
-    mouvement* lejeu;//La suite du mouvement constituant un jeu
-    int score;//Le score du jeu
+    mouvement* lejeu; // La suite du mouvement constituant un jeu
+    int score;        // Le score du jeu
     int copy_damier[NB_CASES][NB_CASES];
-    struct noeud* svt;//pointeur sur les mouvements frere
-    struct noeud* sous_jeu;//pointeur sur les mouvements derives
-}noeud;
+    struct noeud* svt;      // pointeur sur les mouvements frere
+    struct noeud* sous_jeu; // pointeur sur les mouvements derives
+} noeud;
 
 typedef enum typeMouv
 {
     SIMPLE,
     COMPLEXE
-}typeMouvement;
+} typeMouvement;
 
-
-void Afficher_Arbre_horizotalement(mouvement *Arbre, int Niv)
+void Afficher_Arbre_horizotalement(mouvement* Arbre, int Niv)
 {
     int esp;   // Pour l'affichage des espaces
     if (Arbre) // Condition d'arret
@@ -27,7 +26,7 @@ void Afficher_Arbre_horizotalement(mouvement *Arbre, int Niv)
         printf("\n");
         for (esp = 0; esp < Niv; esp++)
             printf("\t");
-        printf("x :%d  y :%d ida :%d ", Arbre->x,Arbre->y,Arbre->IDa);
+        printf("x :%d  y :%d ida :%d ", Arbre->x, Arbre->y, Arbre->IDa);
         // Affichage des fils gauches
         Afficher_Arbre_horizotalement(Arbre->fils1, Niv + 1);
     }
@@ -35,17 +34,14 @@ void Afficher_Arbre_horizotalement(mouvement *Arbre, int Niv)
 
 /*____________________________________________________________*/
 
-
-
-
 /*
-	Nom Fonction : creeNoeud
+    Nom Fonction : creeNoeud
 
-	Entree : _rien
+    Entree : _rien
 
-	Sortie : _element de type noeud
+    Sortie : _element de type noeud
 
-	Description : cree un element de type noeud et le retourne
+    Description : cree un element de type noeud et le retourne
 */
 noeud* creeNoeud(void)
 {
@@ -64,14 +60,14 @@ noeud* creeNoeud(void)
     return (noeud*)NE;
 }
 /*
-	Nom Fonction : creer_mouvement
+    Nom Fonction : creer_mouvement
 
-	Entree : _entiers x et y qui sont les coordonee
-			 _entiers IDj et IDa qui sont les id des jetons
+    Entree : _entiers x et y qui sont les coordonee
+             _entiers IDj et IDa qui sont les id des jetons
 
-	Sortie : _element de type mouvement
+    Sortie : _element de type mouvement
 
-	Description : Cree et retourne un element de type mouvement
+    Description : Cree et retourne un element de type mouvement
 */
 
 mouvement* creer_mouvement(int x, int y, int IDj, int IDa)
@@ -89,26 +85,25 @@ mouvement* creer_mouvement(int x, int y, int IDj, int IDa)
     NE->y = y;
     NE->IDj = IDj;
     NE->IDa = IDa;
-    NE->fils1=NE->fils2 =NE->fils3= NULL;
+    NE->fils1 = NE->fils2 = NE->fils3 = NULL;
     return (mouvement*)NE;
-
 }
 /*____________________________________________________________*/
 
 /*
-	Nom Fonction : insererListeMouvement
+    Nom Fonction : insererListeMouvement
 
-	Entree : _la liste de mouvements
-			 _entiers x et y qui sont les coordonees
-			 _entiers IDj et IDa qui sont les id des jetons
+    Entree : _la liste de mouvements
+             _entiers x et y qui sont les coordonees
+             _entiers IDj et IDa qui sont les id des jetons
 
-	Sortie : _element de type mouvement
+    Sortie : _element de type mouvement
 
-	Description : Cree un element de type mouvement et l'ajoute
-			a la fin de la liste passee en parametre
+    Description : Cree un element de type mouvement et l'ajoute
+            a la fin de la liste passee en parametre
 */
 
-mouvement* insererListeMouvement(mouvement* liste, mouvement *NE)
+mouvement* insererListeMouvement(mouvement* liste, mouvement* NE)
 {
 
     mouvement* ptr = NULL;
@@ -119,21 +114,18 @@ mouvement* insererListeMouvement(mouvement* liste, mouvement *NE)
         ptr = ptr->fils3;
     ptr->fils3 = NE;
     return (mouvement*)liste;
-
 }
 
 /*____________________________________________________________*/
 
-
-
 /*
-	Nom Fonction : copierDamier
+    Nom Fonction : copierDamier
 
-	Entree : _Deux marices de type entiers
+    Entree : _Deux marices de type entiers
 
-	Sortie : _Rien
+    Sortie : _Rien
 
-	Description : Copie une matrice d'entier dans un autre
+    Description : Copie une matrice d'entier dans un autre
 */
 void copierDamier(int DamierDst[NB_CASES][NB_CASES],
                   int DamierSrc[NB_CASES][NB_CASES])
@@ -141,19 +133,17 @@ void copierDamier(int DamierDst[NB_CASES][NB_CASES],
     for (int i = 0; i < NB_CASES; i++)
         for (int j = 0; j < NB_CASES; j++)
             DamierDst[i][j] = DamierSrc[i][j];
-
 }
 /*____________________________________________________________*/
 
-
 /*
-	Nom Fonction : copierLesJetons
+    Nom Fonction : copierLesJetons
 
-	Entree : _Deux marices de type pion
+    Entree : _Deux marices de type pion
 
-	Sortie : _Rien
+    Sortie : _Rien
 
-	Description : Copie une matrice de pion dans un autre
+    Description : Copie une matrice de pion dans un autre
 */
 void copierLesJetons(pion tabDst[NB_PIONS],
                      pion tabSrc[NB_CASES])
@@ -164,199 +154,190 @@ void copierLesJetons(pion tabDst[NB_PIONS],
         tabDst[i].x = tabSrc[i].x;
         tabDst[i].y = tabSrc[i].y;
     }
-
 }
 /*____________________________________________________________*/
 
-
 /*
-	Nom Fonction : MettreAjour_tab_pion
+    Nom Fonction : MettreAjour_tab_pion
 
-	Entree : _tableau de pions
-			 _variable de type match
+    Entree : _tableau de pions
+             _variable de type match
 
-	Sortie : _rien
+    Sortie : _rien
 
-	Description : regarde le jeu qui est dans la
+    Description : regarde le jeu qui est dans la
 */
-void MettreAjour_tab_pion(pion lespions[NB_PIONS],int Damier[NB_CASES][NB_CASES])
+void MettreAjour_tab_pion(pion lespions[NB_PIONS], int Damier[NB_CASES][NB_CASES])
 {
     int i, j, id;
     /*On change l'etat de capture a pas encore capture
-    * pour tous les pions que nous allons trouver encore sur
-    * le damier
-    */
+     * pour tous les pions que nous allons trouver encore sur
+     * le damier
+     */
     for (i = 0; i < NB_CASES; i++)
     {
         for (j = 0; j < NB_CASES; j++)
         {
-            //On recupere le pion qui est a la position acctuel
+            // On recupere le pion qui est a la position acctuel
             id = Damier[i][j];
-            //Si un pion existe sur la position actuel
+            // Si un pion existe sur la position actuel
             if (id >= 0)
             {
-                //On change aussi sa position
+                // On change aussi sa position
                 lespions[id].x = j;
                 lespions[id].y = i;
             }
         }
     }
-
 }
 
 /*____________________________________________________________*/
 
 /*
-	Nom Fonction : deplacerJeton
+    Nom Fonction : deplacerJeton
 
-	Entree : _un mouvement de type mouvement
-			 _une matrice de type entier
-			 _
+    Entree : _un mouvement de type mouvement
+             _une matrice de type entier
+             _
 
-	Sortie : _Rien
+    Sortie : _Rien
 
-	Description : L'utilite de cette fct c'est qu'elle applique
-			un mouvement sur le damier en mettant a jour la
-			table des jetons
+    Description : L'utilite de cette fct c'est qu'elle applique
+            un mouvement sur le damier en mettant a jour la
+            table des jetons
 */
 
-void deplacerJeton(mouvement *lemvt,
+void deplacerJeton(mouvement* lemvt,
                    int Damier[NB_CASES][NB_CASES], pion lesjetons[NB_PIONS])
 {
     if (lemvt->IDa >= 0)
     {
         lesjetons[lemvt->IDa].etat = 0;
-        //On sauvgarde la position du jeton de l'adversaire
+        // On sauvgarde la position du jeton de l'adversaire
         Damier[lesjetons[lemvt->IDa].y][lesjetons[lemvt->IDa].x] = -1;
-        //On reinitialise les donnee dans la table des jettons
-
+        // On reinitialise les donnee dans la table des jettons
     }
-    //On enleve le jeton du joeur de sa position initiale
+    // On enleve le jeton du joeur de sa position initiale
     Damier[lesjetons[lemvt->IDj].y][lesjetons[lemvt->IDj].x] = -1;
-    //On le deplace vers la nouvelle position
+    // On le deplace vers la nouvelle position
     Damier[lemvt->y][lemvt->x] = lemvt->IDj;
-    //On reinitialise les donnee dans la table des jettons
+    // On reinitialise les donnee dans la table des jettons
     lesjetons[lemvt->IDj].x = lemvt->x;
     lesjetons[lemvt->IDj].y = lemvt->y;
-    if(lemvt->IDj < 12)
+    if (lemvt->IDj < 12)
     {
-        if(lesjetons[lemvt->IDj].y == 7)
-            lesjetons[lemvt->IDj].etat=2;
+        if (lesjetons[lemvt->IDj].y == 7)
+            lesjetons[lemvt->IDj].etat = 2;
     }
     else
     {
-        if(lesjetons[lemvt->IDj].y==0)
-            lesjetons[lemvt->IDj].etat=2;
+        if (lesjetons[lemvt->IDj].y == 0)
+            lesjetons[lemvt->IDj].etat = 2;
     }
     MettreAjour_tab_pion(lesjetons, Damier);
 }
 
-
 /*
  * Fonction qui determine tous les mouvements possibles pour un pion ordinaire
  */
-mouvement *Mouvements(int id,int damier[NB_CASES][NB_CASES],
-                      pion pions[NB_PIONS],mouvement *N,int v,
-                      int h,int typemvt)
+mouvement* Mouvements(int id, int damier[NB_CASES][NB_CASES],
+                      pion pions[NB_PIONS], mouvement* N, int v,
+                      int h, int typemvt)
 {
-    //déclaration des variables
+    // déclaration des variables
     int damier1[NB_CASES][NB_CASES];
     pion pions1[NB_PIONS];
-    int x=pions[id].x;
-    int y=pions[id].y;
-        if(x+h<8 &&y+v<8 && x+h>=0 &&y+v>=0)//s'il a une case libre pour boujer
+    int x = pions[id].x;
+    int y = pions[id].y;
+    if (x + h < 8 && y + v < 8 && x + h >= 0 && y + v >= 0) // s'il a une case libre pour boujer
+    {
+        if (damier[y + v][x + h] == -1 && typemvt == 0)
+            // s'il va faire une mouvement sans capturer un pion adversaire
+            N = creer_mouvement(x + h, y + v, id, -1);
+        // s'il a un pion adversaire à coté
+        else if ((damier[y + v][x + h] < 12 && damier[y][x] > 11) || (damier[y + v][x + h] > 11 && damier[y][x] < 12))
         {
-            if(damier[y+v][x+h]==-1 &&typemvt ==0)
-                //s'il va faire une mouvement sans capturer un pion adversaire
-                N = creer_mouvement(x + h, y + v, id, -1);
-            //s'il a un pion adversaire à coté
-            else if((damier[y+v][x+h]<12 && damier[y][x]>11)
-                    || (damier[y+v][x+h]>11 && damier[y][x]<12) )
+            // s'il peut capurer cette pion de l'adversaire
+            if (damier[y + (v * 2)][x + (h * 2)] == -1)
             {
-                //s'il peut capurer cette pion de l'adversaire
-                if(damier[y+(v*2)][x+(h*2)]==-1)
-                {
-                    //on garde cette mouvement
-                    N=creer_mouvement(x+(h*2),y+(v*2),id,damier[y+v][x+h]);
-                    //on fait une copie du damier et on applique cette mouvement
-                    copierDamier(damier1,damier);
-                    copierLesJetons(pions1,pions);
-                    deplacerJeton(N,damier1,pions1);
-                    //on cherche les mouvements complexes à droite et à gauche
-                    N->fils1 = Mouvements(id, damier1, pions1, N->fils1, v,-1,COMPLEXE);
-                    N->fils2 = Mouvements(id, damier1, pions1, N->fils2, v,1,COMPLEXE);
-                }
+                // on garde cette mouvement
+                N = creer_mouvement(x + (h * 2), y + (v * 2), id, damier[y + v][x + h]);
+                // on fait une copie du damier et on applique cette mouvement
+                copierDamier(damier1, damier);
+                copierLesJetons(pions1, pions);
+                deplacerJeton(N, damier1, pions1);
+                // on cherche les mouvements complexes à droite et à gauche
+                N->fils1 = Mouvements(id, damier1, pions1, N->fils1, v, -1, COMPLEXE);
+                N->fils2 = Mouvements(id, damier1, pions1, N->fils2, v, 1, COMPLEXE);
             }
         }
-        //retourner le noeud des mouvements
+    }
+    // retourner le noeud des mouvements
     return ((mouvement*)N);
 }
 /*
-* Fonction qui determine tous les mouvements possibles pour un pion dame
-*/
-mouvement *Dame(int id,int damier[NB_CASES][NB_CASES], pion pions[NB_PIONS],mouvement *N,int v,int h,int typemvt)
+ * Fonction qui determine tous les mouvements possibles pour un pion dame
+ */
+mouvement* Dame(int id, int damier[NB_CASES][NB_CASES], pion pions[NB_PIONS], mouvement* N, int v, int h, int typemvt)
 {
-    //déclaration des variabes
+    // déclaration des variabes
     int damier1[NB_CASES][NB_CASES];
     pion pions1[NB_PIONS];
-    int a,b,c,d;
-    int x=pions[id].x;
-    int y=pions[id].y;
-    mouvement *T,*T2;
-    a=x+h;
-    b=y+v;
-    //tant qu'il y'a des cases dans le damier
-    while(a<8 && b<8 && a>=0 && b>=0)
+    int a, b, c, d;
+    int x = pions[id].x;
+    int y = pions[id].y;
+    mouvement *T, *T2;
+    a = x + h;
+    b = y + v;
+    // tant qu'il y'a des cases dans le damier
+    while (damier[b][a] == -1 && typemvt == SIMPLE)
     {
-        if(damier[b][a]==-1 &&typemvt ==SIMPLE)//si la case est libre
+        if (a < 8 && b < 8 && a >= 0 && b >= 0) // si la case est libre
         {
-            T= creer_mouvement(a, b, id, -1);
-            N= insererListeMouvement(N,T);
-            a=a+h;
-            b=b+v;
+            T = creer_mouvement(a, b, id, -1);
+            N = insererListeMouvement(N, T);
+            a = a + h;
+            b = b + v;
         }
         else
+            break;
+    }
+
+    while (damier[b][a] == -1 && typemvt == COMPLEXE)
+    {
+        if (a < 8 && b < 8 && a >= 0 && b >= 0)
         {
-            if ((damier[b][a] < 12 && damier[b][a] >= 0 && damier[y][x] > 11) || (damier[b][a] > 11 && damier[y][x] < 12 && damier[y][x] >= 0))
+            a = a + h;
+            b = b + v;
+        }
+    }
+    if ((damier[b][a] < 12 && damier[b][a] >= 0 && damier[y][x] > 11) || (damier[b][a] > 11 && damier[y][x] < 12 && damier[y][x] >= 0))
+    {
+        c = a;
+        d = b;
+        c = c + h;
+        d = d + v;
+        while (damier[d][c] == -1)
+        {
+            if (c < 8 && d < 8 && c >= 0 && d >= 0)
             {
-                    c = a;
-                    d = b;
-                    c = c + h;
-                    d = d + v;
-                    while (c < 8 && d < 8 && c >= 0 && d >= 0)
-                    {
-                        if (damier[d][c] == -1)
-                        {
-                            typemvt = 1;
-                            T = creer_mouvement(c, d, id, damier[b][a]);
-                            T2 = creer_mouvement(c, d, id, damier[b][a]);
-                            copierDamier(damier1, damier);
-                            copierLesJetons(pions1, pions);
-                            deplacerJeton(T, damier1, pions1);
-                            T->fils1 = Dame(id, damier1, pions1, T->fils1, v, -1, COMPLEXE);
-                            T->fils2 = Dame(id, damier1, pions1, T->fils2, v, 1, COMPLEXE);
-                            T2->fils1 = Dame(id, damier1, pions1, T2->fils1, v * (-1), h, COMPLEXE);
-                            N = insererListeMouvement(N, T);
-                            if (T2->fils1)
-                                N = insererListeMouvement(N, T2);
-                            c = c + h;
-                            d = d + v;
-                        }
-                        else
-                        {
-                            a = 19;
-                            break;
-                        }
-                            
-                    }
+                typemvt = 1;
+                T = creer_mouvement(c, d, id, damier[b][a]);
+                T2 = creer_mouvement(c, d, id, damier[b][a]);
+                copierDamier(damier1, damier);
+                copierLesJetons(pions1, pions);
+                deplacerJeton(T, damier1, pions1);
+                T->fils1 = Dame(id, damier1, pions1, T->fils1, v, 1, COMPLEXE);
+                T->fils2 = Dame(id, damier1, pions1, T->fils2, v, -1, COMPLEXE);
+                T2->fils1 = Dame(id, damier1, pions1, T2->fils1, v * (-1), h, COMPLEXE);
+                N = insererListeMouvement(N, T);
+                if (T2->fils1)
+                    N = insererListeMouvement(N, T2);
+                c = c + h;
+                d = d + v;
             }
             else
-            {
-                    break;
-            }
-                    
-            a=a+h;
-            b=b+v;
+                break;
         }
     }
     return ((mouvement*)N);
@@ -364,41 +345,42 @@ mouvement *Dame(int id,int damier[NB_CASES][NB_CASES], pion pions[NB_PIONS],mouv
 /*
  * Fonction qui determine tous les mouvements possibles pour un pion quelconque
  */
-noeud *Mouvements_Possibles(int id,int damier[NB_CASES][NB_CASES], pion pions[NB_PIONS])
+noeud* Mouvements_Possibles(int id, int damier[NB_CASES][NB_CASES], pion pions[NB_PIONS])
 {
     int v;
-    noeud *A=creeNoeud();
-    noeud *tmp;
-    tmp=A;
-    if(pions[id].etat ==2)//s'il s'agit d'un dame
+    noeud* A = creeNoeud();
+    noeud* tmp;
+    tmp = A;
+    if (pions[id].etat == 2) // s'il s'agit d'un dame
     {
-        //on cherche les mouvements possibles dans les 4 directions
-        tmp->lejeu= Dame(id,damier,pions,tmp->lejeu,1,-1,SIMPLE);// sense verticale = 1 et sens horizontale =-1
+        // on cherche les mouvements possibles dans les 4 directions
+        tmp->lejeu = Dame(id, damier, pions, tmp->lejeu, 1, -1, SIMPLE); // sense verticale = 1 et sens horizontale =-1
 
-        tmp->svt=creeNoeud();
-        tmp=tmp->svt;
-        tmp->lejeu= Dame(id,damier,pions,tmp->lejeu,1,1,SIMPLE);// sense verticale = 1 et sens horizontale =1
+        tmp->svt = creeNoeud();
+        tmp = tmp->svt;
+        tmp->lejeu = Dame(id, damier, pions, tmp->lejeu, 1, 1, SIMPLE); // sense verticale = 1 et sens horizontale =1
 
-        tmp->svt=creeNoeud();
-        tmp=tmp->svt;
-        tmp->lejeu= Dame(id,damier,pions,tmp->lejeu,-1,-1,SIMPLE);// sense verticale = -1 et sens horizontale =-1
+        tmp->svt = creeNoeud();
+        tmp = tmp->svt;
+        tmp->lejeu = Dame(id, damier, pions, tmp->lejeu, -1, -1, SIMPLE); // sense verticale = -1 et sens horizontale =-1
 
-        tmp->svt=creeNoeud();
-        tmp=tmp->svt;
-        tmp->lejeu= Dame(id,damier,pions,tmp->lejeu,-1,1,SIMPLE);// sense verticale = -1 et sens horizontale =1
+        tmp->svt = creeNoeud();
+        tmp = tmp->svt;
+        tmp->lejeu = Dame(id, damier, pions, tmp->lejeu, -1, 1, SIMPLE); // sense verticale = -1 et sens horizontale =1
     }
-    else//sinon
+    else // sinon
     {
-        //on determine le sens vertical du joueur
-        if(id<12) v=1;
+        // on determine le sens vertical du joueur
+        if (id < 12)
+            v = 1;
         else
-            v=-1;
-        //v :sens vertical pour un joueur, sens horizontale =1
-        tmp->lejeu= Mouvements(id,damier,pions, tmp->lejeu,v,1,SIMPLE);
-        //v :sense vertical pour un joueur, sens horizontale =-1
-        tmp->svt=creeNoeud();
-        tmp=tmp->svt;
-        tmp->lejeu=Mouvements(id,damier,pions,tmp->lejeu,v,-1,SIMPLE);
+            v = -1;
+        // v :sens vertical pour un joueur, sens horizontale =1
+        tmp->lejeu = Mouvements(id, damier, pions, tmp->lejeu, v, 1, SIMPLE);
+        // v :sense vertical pour un joueur, sens horizontale =-1
+        tmp->svt = creeNoeud();
+        tmp = tmp->svt;
+        tmp->lejeu = Mouvements(id, damier, pions, tmp->lejeu, v, -1, SIMPLE);
     }
-    return ((noeud *)A);
+    return ((noeud*)A);
 }
